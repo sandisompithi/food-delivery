f51ea25b478b74b94dec903116daf595a980e423
sandisompithi <sandisompithi@outlook.com>
2020-03-19 15:05:05 +0200

    implementing Item class

---
+++ food-delivery/src/main/java/com/reverside/sandiso/model/Item.java
@@ -0,0 +1,51 @@
+package com.reverside.sandiso.model;
+
+import javax.persistence.Entity;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Item {
+
+	private Long itemId;
+	private String itemName;
+	private Double price;
+	private int quantity;
+	
+	@ManyToOne
+	@JoinColumn(name = "id")
+	private Restaurants restaurants;
+	
+	public Long getItemId() {
+		return itemId;
+	}
+	public void setItemId(Long itemId) {
+		this.itemId = itemId;
+	}
+	public String getItemName() {
+		return itemName;
+	}
+	public void setItemName(String itemName) {
+		this.itemName = itemName;
+	}
+	public Double getPrice() {
+		return price;
+	}
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	public Restaurants getRestaurants() {
+		return restaurants;
+	}
+	public void setRestaurants(Restaurants restaurants) {
+		this.restaurants = restaurants;
+	}
+	
+	
+}
--- food-delivery/src/main/java/com/reverside/sandiso/model/Restaurants.java
+++ food-delivery/src/main/java/com/reverside/sandiso/model/Restaurants.java
@@ -1,11 +1,17 @@
 package com.reverside.sandiso.model;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 
@@ -19,6 +25,9 @@ public class Restaurants {
 	private String name;
 	
 	private String city;
+	
+	@OneToMany(mappedBy = "restaurants", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+	private List<Item> items = new ArrayList<>();
 
 	@ManyToOne
 	@JoinColumn(name = "user_id")
@@ -47,6 +56,15 @@ public class Restaurants {
 
 	public void setCity(String city) {
 		this.city = city;
+	}
+	
+
+	public List<Item> getItems() {
+		return items;
+	}
+
+	public void setItems(List<Item> items) {
+		this.items = items;
 	}
 
 	public User getUser() {
